ba3ffb8dfe7b3c94f44ff6e4f186f90149f30005
Adamearth <517364792@qq.com>
2019-02-25 18:54:34 +0800

    fix

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # adamearthhuang.github.io
 
-利用 Github 服务器对域名请求进行跳转。
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: '黄宝权',
+  sidebarDepth: 2,
+  themeConfig: {
+    sidebar: {
+      '/': [
+        {
+          title: '前端',
+          children: [
+            '/test1/1',
+          ]
+        },
+        {
+          title: '后端',
+          children:[
+            '/test2/2',
+          ]
+        },
+      ],
+    }, 
+ 
+  }, 
+}
